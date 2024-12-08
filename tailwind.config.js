6d8c766f0bc6a6b26844110fb4913bf37960c308
Dani Rana <danirana8604@gmail.com>
2024-12-08 22:02:50 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Add all relevant paths
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
